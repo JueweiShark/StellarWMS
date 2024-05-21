fa10a20bd0dacb01a6754fd2db2dbf5b2982b04f
KITIShark <tooko1517@outlook.com>
2024-05-21 11:44:04 +0800

    1.0

---
+++ src/main/java/com/example/wmsspringbootproject/WmsSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.wmsspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsSpringbootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsSpringbootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wmsspringbootproject/WmsSpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wmsspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsSpringbootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
