1a4a84174f6d93b88926bc910c71fb6c32ecd806
InitiateAutumn <148079504+InitiateAutumn@users.noreply.github.com>
2024-06-06 09:09:47 +0800

    4.01 权限管理初始版本
    Signed-off-by: InitiateAutumn <148079504+InitiateAutumn@users.noreply.github.com>

--- src/main/java/com/example/wmsspringbootproject/model/entity/Product.java
+++ src/main/java/com/example/wmsspringbootproject/model/entity/Product.java
@@ -2,7 +2,9 @@ package com.example.wmsspringbootproject.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
 
+@Data
 public class Product {
     @TableId(type= IdType.AUTO)
     private Integer id;
--- src/test/java/com/example/wmsspringbootproject/WmsSpringbootProjectApplicationTests.java
+++ src/test/java/com/example/wmsspringbootproject/WmsSpringbootProjectApplicationTests.java
@@ -3,11 +3,17 @@ package com.example.wmsspringbootproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Field;
+
 @SpringBootTest
 class WmsSpringbootProjectApplicationTests {
 
     @Test
     void contextLoads() {
+        String message = "Hello World!";
+        for (Field declaredField : message.getClass().getDeclaredFields()) {
+            System.out.println(declaredField.getName());
+        }
     }
 
 }
